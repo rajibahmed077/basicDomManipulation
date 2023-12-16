da104171e1495a2816cac8aa7cadbc4e2bf4aaef
rajib-ahmed <rajibahmedseo@gmail.com>
2023-12-16 18:27:27 +0600

    change the background color

---
+++ script.js
@@ -0,0 +1,10 @@
+let fruits = ["apple", "banana", "orange"];
+let myfavourite = [...fruits,'mango','pineapple'];
+
+myfavourite.forEach(fruit => {
+    console.log(fruit);
+});
+
+myfavourite.map(fruit => {
+    
+})
